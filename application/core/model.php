a7fbd63ccf6da932e0a4c68409b4d5de34cb886d
bigturtle88 <bigturtle@i.ua>
2015-10-13 11:37:53 +0300

    Create model.php

---
+++ application/core/model.php
@@ -0,0 +1,16 @@
+<?php
+class Model
+{
+				public $dbh;
+				public $row;
+				public $result;
+				public $data = array();
+				
+				function __construct( )
+				{
+								require_once( 'db.php' );
+								$this->dbh = new DB_Mysql();
+				}
+				
+}
+?>
