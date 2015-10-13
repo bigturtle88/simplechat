0e544b57bc52017ae51f402f6a8624c88edbaa62
bigturtle88 <bigturtle@i.ua>
2015-10-13 11:28:52 +0300

    Create test123.sql

---
+++ test123.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Окт 12 2015 г., 14:03
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `test123`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `channels`
+--
+
+CREATE TABLE IF NOT EXISTS `channels` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `channels`
+--
+
+INSERT INTO `channels` (`id`, `name`) VALUES
+(2, 'Genersl chat');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `text` varchar(40) NOT NULL,
+  `channel` int(10) unsigned NOT NULL,
+  `recipient` int(11) NOT NULL,
+  `from` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+
+--
+-- Дамп данных таблицы `messages`
+--
+
+INSERT INTO `messages` (`id`, `text`, `channel`, `recipient`, `from`, `date`) VALUES
+(29, 'hi!', 2, 0, 9, '2015-10-12 11:02:06'),
+(30, 'Hello', 2, 0, 13, '2015-10-12 11:02:38');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `idUser` int(11) NOT NULL,
+  `idChannel` int(11) NOT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `room`
+--
+
+INSERT INTO `room` (`idUser`, `idChannel`) VALUES
+(10, 1),
+(12, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(40) COLLATE utf8_bin NOT NULL,
+  `pass` varchar(40) COLLATE utf8_bin NOT NULL,
+  `key` varchar(40) COLLATE utf8_bin NOT NULL,
+  `group` varchar(40) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=14 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `pass`, `key`, `group`) VALUES
+(3, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'NULL ', '1'),
+(9, 'user123', '95c946bf622ef93b0a211cd0fd028dfdfcf7e39e', 'NULL ', '2'),
+(13, 'user321', 'c63b412d9a0e86e8f1ad86c2708a864877126dd8', 'NULL ', '2');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
