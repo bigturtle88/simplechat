742d8a822df3532882fea5ef1ace765adfa5c1d2
bigturtle88 <bigturtle@i.ua>
2015-10-13 12:01:58 +0300

    Create redirect.php

---
+++ application/views/redirect.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title><?php echo $content['title'];?></title>
+
+	<link rel="stylesheet" href="<?php echo SITE_URL;?>application/views/bootstrap/css/bootstrap.css" type="text/css">
+	 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,300" type="text/css">
+	 <meta http-equiv="refresh" content="1;URL=<?php echo $content['url'];?>" />
+</head>
+<body>
+</body>
+</html>
