2305dee9ee4cb52e750a1f03733d6f7e7e4f71b8
bigturtle88 <bigturtle@i.ua>
2015-10-13 12:04:29 +0300

    Create user.php

---
+++ application/views/user.php
@@ -0,0 +1,46 @@
+
+<body>
+
+<div class="container-fluid col-md-8 col-md-offset-2">
+
+<header><a href="<?php echo SITE_URL;?>"><img id="logo" src="<?php echo SITE_URL;?>application/views/img/logo.jpg" alt="Whitesquare logo"></a>
+	
+
+	</header>
+
+	
+	 <nav class="navbar navbar-default">
+
+<ul class="nav navbar-nav">
+   <li><a href="<?php echo SITE_URL;?>">Home</a> </li>
+ <li><a href="<?php echo SITE_URL;?>edit">Edit</a></li> 
+ <li><a href="<?php echo SITE_URL;?>chat">Chat</a></li>
+ <li><a href="<?php echo SITE_URL;?>main/exit">Exit</a></li>
+ <ul>
+	 </nav>
+<div class="heading"></div>
+	<div class="row">
+		<aside class="col-md-17"></aside>
+		 <section class="col-md-10">	
+			
+<pre>Login: <?php echo  $content[ 'login' ]; ?></pre>
+
+											
+ 
+ 
+			 
+			 
+  
+ 
+</section>
+
+		</div>
+
+		<footer></footer>
+			</div>
+</body>
+</html>
+
+
+
+
