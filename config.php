cb0bb630b132b23daf098ab503340fdf77179e0c
bigturtle88 <bigturtle@i.ua>
2015-10-13 11:26:14 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define( 'SITE_URL', 'http://localhost/simplechat'.'/' );
+define( 'DB_NAME', 'test123' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASS', '' );
+define( 'ADMIN_DIR', 'root' );
+?>
