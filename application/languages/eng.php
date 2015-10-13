9dfcde8b6b0565f0fcb0d9f5c3c5aa1e395d1437
bigturtle88 <bigturtle@i.ua>
2015-10-13 11:41:17 +0300

    Create eng.php

---
+++ application/languages/eng.php
@@ -0,0 +1,11 @@
+<?php
+//error
+define( 'ERROR_LOGIN', 'Incorrect login.' );
+define( 'NOT_THE_CORRECT_PASSWORD', 'Not the correct password.' );
+define( 'NOT_CORRECT_NEW_PASSWORD', 'Not correct new password');
+define( 'THE_PASSWORDS_DO_NOT_MATCH', 'The passwords do not match');
+define( 'ERROR_LOGIN_OR_PASS', 'Incorrect login or password.' );
+
+define( 'REGISTRATION_COMPLETED', 'Registration completed successfully.' );
+define( 'EDIT_PASSWORD', 'The password is successfully changed.');
+?>
