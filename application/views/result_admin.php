e4dd962e1dabec2745d81888a3c1fe34ad604181
bigturtle88 <bigturtle@i.ua>
2015-10-13 12:03:28 +0300

    Create result_admin.php

---
+++ application/views/result_admin.php
@@ -0,0 +1,37 @@
+
+<body>
+
+<div class="container-fluid col-md-8 col-md-offset-2">
+<header><a href="<?php echo SITE_URL;?>"><img id="logo" src="<?php echo SITE_URL;?>application/views/img/logo.jpg" alt="Whitesquare logo"></a>
+	
+
+	</header>
+
+	
+	 <nav class="navbar navbar-default">
+
+<ul class="nav navbar-nav">
+   <li><a href="<?php echo SITE_URL;?>">Home</a> </li>
+
+ <ul>
+	 </nav>
+<div class="heading"></div>
+	<div class="row">
+		
+			<aside class="col-md-17"></aside>
+		 <section class="col-md-10">	
+
+		<h3> <span class="label label-primary">
+		 <?php
+
+echo $content['result'];
+?>
+ </span></h3>
+</section>
+
+		</div>
+
+		<footer></footer>
+			</div>
+</body>
+</html>
