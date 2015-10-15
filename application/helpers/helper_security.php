90ae929c9163c683584748a0cfbecdbb8299fc2f
bigturtle88 <bigturtle@i.ua>
2015-10-15 08:59:33 +0300

    Update helper_security.php

--- application/helpers/helper_security.php
+++ application/helpers/helper_security.php
@@ -1,48 +1,26 @@
 <?php
-class Security
-{
-	public $data;
-	
-	public function __construct(){
-	}
-	
-	public function filter($data){
-	
-	if( isset ($data) ){
-	
-		$data = addslashes ($data);
-		$data = substr($data,0,40);
-		return $data;
-		}
-	
-	return NULL;
-	
-	
-	
-	
-	}							
-
-	public function filter_session($data){
-	
-	if( is_array ($data) ){
-	
-	if(isset ($data['id']) and isset ($data['key'])){
-
-
-	$data['id'] = addslashes ($data['id']);
-	$data['key'] = addslashes ($data['key']);
-	
-	return $data;
-	}
-		
-	return NULL;	
-		}
-	
-	return NULL;
-	
-	
-	
-	
-	}
+class Security {
+    public $data;
+    public function __construct() {
+    }
+    public function filter($data) {
+        if (isset($data)) {
+            $data = addslashes($data);
+            $data = substr($data, 0, 40);
+            return $data;
+        }
+        return NULL;
+    }
+    public function filter_session($data) {
+        if (is_array($data)) {
+            if (isset($data['id']) and isset($data['key'])) {
+                $data['id'] = addslashes($data['id']);
+                $data['key'] = addslashes($data['key']);
+                return $data;
+            }
+            return NULL;
+        }
+        return NULL;
+    }
 }
-?>
+?> 
