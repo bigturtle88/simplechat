1c70e3f45e4c19da0d502a035fe3ba46a1be7f5d
bigturtle88 <bigturtle@i.ua>
2015-10-15 08:52:32 +0300

    Update controller_delete.php

--- application/controllers/controller_delete.php
+++ application/controllers/controller_delete.php
@@ -1,27 +1,22 @@
 <?php
-
-require_once("application/models/model_user.php");
-
-class Controller_delete extends Controller
-{
-				public $model;
-				public $view;
-				public $data = array( );
-				function __construct( )
-				{
-								$this->view  = new View();
-								$this->model = new Model_user();
-				}
-				public function action_index( )
-				{
-								$login = $_SESSION[ 'valid_user' ];
-								$this->model->delete( $login );
-								unset( $_SESSION[ 'valid_user' ] );
-								session_destroy();
-								$data[ 'result' ] = 'Пользователь удален!';
-								$this->view->generate( $data, 'head.php' );
-								$this->view->generate( $data, 'result.php' );
-								$this->view->generate( $data, 'footer.php' );
-				}
+require_once ("application/models/model_user.php");
+class Controller_delete extends Controller {
+    public $model;
+    public $view;
+    public $data = array();
+    function __construct() {
+        $this->view = new View();
+        $this->model = new Model_user();
+    }
+    public function action_index() {
+        $login = $_SESSION['valid_user'];
+        $this->model->delete($login);
+        unset($_SESSION['valid_user']);
+        session_destroy();
+        $data['result'] = '&#1055;&#1086;&#1083;&#1100;&#1079;&#1086;&#1074;&#1072;&#1090;&#1077;&#1083;&#1100; &#1091;&#1076;&#1072;&#1083;&#1077;&#1085;!';
+        $this->view->generate($data, 'head.php');
+        $this->view->generate($data, 'result.php');
+        $this->view->generate($data, 'footer.php');
+    }
 }
 ?>
