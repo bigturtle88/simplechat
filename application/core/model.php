e54e160cb2092c1c66a285619fd10758c3ddeb86
bigturtle88 <bigturtle@i.ua>
2015-10-15 08:57:40 +0300

    Update model.php

--- application/core/model.php
+++ application/core/model.php
@@ -1,16 +1,12 @@
 <?php
-class Model
-{
-				public $dbh;
-				public $row;
-				public $result;
-				public $data = array();
-				
-				function __construct( )
-				{
-								require_once( 'db.php' );
-								$this->dbh = new DB_Mysql();
-				}
-				
+class Model {
+    public $dbh;
+    public $row;
+    public $result;
+    public $data = array();
+    function __construct() {
+        require_once ('db.php');
+        $this->dbh = new DB_Mysql();
+    }
 }
-?>
+?> 
